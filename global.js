024a0fa68a4db223b8c0115ede456706490aa84c
Mori <classactivitycalculator@gmail.com>
2019-05-07 12:06:37 +0430

    Update global.js

--- global.js
+++ global.js
@@ -1,36 +1,36 @@
-    var form = document.querySelector('form');
+var form = document.querySelector('form');
 
-    function averageInputValues(fieldset) {
-      var totalValue = 0;
-      var totalNumber = 0;
-      var inputs = fieldset.querySelectorAll('input');
-      for (var input of inputs) {
-        if (!input.validity.valid) {
-          return;
-        }
-        totalValue += Number(input.value);
-        totalNumber += Boolean(input.value);
-      }
-      return totalValue / totalNumber;
+function averageInputValues(fieldset) {
+  var totalValue = 0;
+  var totalNumber = 0;
+  var inputs = fieldset.querySelectorAll('input');
+  for (var input of inputs) {
+    if (!input.validity.valid) {
+      return;
     }
+    totalValue += Number(input.value);
+    totalNumber += Boolean(input.value);
+  }
+  return totalValue / totalNumber;
+}
 
-    function detectChange() {
-      var inputs = form.querySelectorAll('input');
-      for (var input of inputs) {
-        if (input.value) {
-          return true;
-        }
-      }
+function detectChange() {
+  var inputs = form.querySelectorAll('input');
+  for (var input of inputs) {
+    if (input.value) {
+      return true;
     }
+  }
+}
 
-    form.querySelector('[type="reset"]').addEventListener('click', function(event) {
-      if (detectChange() && !confirm('Your changes will be lost.\nAre you sure you want to reset?')) {
-        event.preventDefault();
-      }
-    });
+form.querySelector('[type="reset"]').addEventListener('click', function(event) {
+  if (detectChange() && !confirm('Your changes will be lost.\nAre you sure you want to reset?')) {
+    event.preventDefault();
+  }
+});
 
-    window.addEventListener('beforeunload', function(event) {
-      if (detectChange()) {
-        event.returnValue = 'Your changes may be lost.';
-      }
-    });
+window.addEventListener('beforeunload', function(event) {
+  if (detectChange()) {
+    event.returnValue = 'Your changes may be lost.';
+  }
+});
